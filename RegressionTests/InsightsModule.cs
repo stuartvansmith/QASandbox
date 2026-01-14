dc4a1c52aebb45d503c4de79860b77ab8778b0cb
Stuart Smith <stuart.smith@originbenefits.ai>
2026-01-14 15:28:26 +0000

    fixed deliberate fail

--- RegressionTests/InsightsModule.cs
+++ RegressionTests/InsightsModule.cs
@@ -28,37 +28,12 @@ namespace QA.AutomationTests.RegressionTests
             // Now you can parse it or upload it as an artifact
             Console.WriteLine($"Downloaded file: {filePath}");
 
-            //var download = await page.RunAndWaitForDownloadAsync(async () =>
-            //{
-            //    var exportBtn = page.GetByText("Export Report").First;
-            //    await exportBtn.WaitForAsync();
-            //    await exportBtn.ClickAsync();
-            //});
-   
-            //await page.PauseAsync();
-            return;
 
-            await page.Locator("#RYc3xSz0vU > .rz-dropdown-label").ClickAsync();
-            await page.GetByRole(AriaRole.Listitem, new() { Name = "Afghanistan (AFG)" }).Locator("div").Nth(1).ClickAsync();
-            await page.GetByRole(AriaRole.Listbox).GetByText("Afghanistan (AFG)").ClickAsync();
-            await page.GetByRole(AriaRole.Listbox).GetByText("Afghanistan (AFG)").ClickAsync();
-            await page.Locator("#i0DYrveBvU > .rz-dropdown-label").ClickAsync();
-            await page.GetByRole(AriaRole.Listitem, new() { Name = "Afghanistan (AFG)" }).ClickAsync();
-            await page.Locator("#QCEQ2j7V4k > .rz-dropdown-label").ClickAsync();
-            await page.GetByRole(AriaRole.Listitem, new() { Name = "Active" }).Locator("div").Nth(1).ClickAsync();
-            await page.Locator("#oCo54gB2QU > .rz-dropdown-label").ClickAsync();
-            await page.GetByRole(AriaRole.Listitem, new() { Name = "Provider One" }).Locator("div").Nth(1).ClickAsync();
-            await page.Locator("#gKdtCk0iIE > .rz-dropdown-trigger > .notranslate").ClickAsync();
-            await page.GetByRole(AriaRole.Listitem, new() { Name = "Risk" }).Locator("div").Nth(1).ClickAsync();
-            await page.Locator("#b5bYY8IAd0 > .rz-dropdown-trigger > .notranslate").ClickAsync();
-            await page.GetByRole(AriaRole.Listitem, new() { Name = "Critical illness" }).Locator("div").Nth(1).ClickAsync();
-            var download1 = await page.RunAndWaitForDownloadAsync(async () =>
-            {
-                await page.GetByRole(AriaRole.Button, new() { Name = "export_notes Export Report" }).ClickAsync();
-            });
-            await page.PauseAsync();   
         }
         [TestMethod]
-        public async Task deliberateFailToTestReport() { }
+        public async Task deliberateFailToTestReport() 
+        {
+            Assert.IsTrue(false, "Deliberatly fail...");
+        }
     }
 }
